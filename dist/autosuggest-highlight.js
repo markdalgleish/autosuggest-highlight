b5f4ecc2cc712f4ce40d63f90e94debf5c1d31c1
markdalgleish <mark.john.dalgleish@gmail.com>
2015-06-22 10:45:08 +1000

    Build dist

--- dist/autosuggest-highlight.js
+++ dist/autosuggest-highlight.js
@@ -3,46 +3,19 @@
 Object.defineProperty(exports, '__esModule', {
   value: true
 });
-'use strict';
 
-require('string.prototype.repeat');
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
-function escapeRegexCharacters(str) {
-  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-}
+var _match = require('./match');
 
-exports['default'] = function (text, query) {
-  var queryWords = query.split(/\s+/).filter(function (queryWord) {
-    return queryWord.length > 0;
-  });
+var _match2 = _interopRequireDefault(_match);
 
-  return queryWords.reduce(function (result, queryWord) {
-    var regex = undefined;
+var _parse = require('./parse');
 
-    if (queryWord === '&') {
-      regex = /&/i;
-    } else if (queryWord[0] === '(') {
-      regex = new RegExp(escapeRegexCharacters(queryWord), 'i');
-    } else {
-      regex = new RegExp('\\b' + escapeRegexCharacters(queryWord), 'i');
-    }
+var _parse2 = _interopRequireDefault(_parse);
 
-    var index = text.search(regex);
-
-    if (index > -1) {
-      result.push([index, index + queryWord.length]);
-
-      // Replace what we just found with spaces so we don't find it again
-      text = text.substring(0, index) + ' '.repeat(queryWord.length) + text.substring(index + queryWord.length);
-    }
-
-    return result;
-  }, []).filter(function (match) {
-    return match !== null;
-  }).sort(function (match1, match2) {
-    return match1[0] - match2[0];
-  });
+exports['default'] = {
+  match: _match2['default'],
+  parse: _parse2['default']
 };
-
 module.exports = exports['default'];
